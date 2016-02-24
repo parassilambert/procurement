1c736822a35e1ae1546d239fe9ad10c4ad0a2b83
parassilambert <parassilambert@hotmail.com>
2016-02-25 01:23:18 +0300

    Merge origin/master
    Conflicts:
    	.gitignore
    	README.md
    	app/AppCache.php
    	app/AppKernel.php
    	app/Resources/views/base.html.twig
    	app/Resources/views/default/index.html.twig
    	app/autoload.php
    	app/config/config.yml
    	app/config/config_dev.yml
    	app/config/config_prod.yml
    	app/config/parameters.yml.dist
    	app/config/routing.yml
    	app/config/routing_dev.yml
    	app/config/security.yml
    	app/config/services.yml
    	composer.json
    	composer.lock
    	nbproject/private/private.properties
    	nbproject/project.properties
    	src/AppBundle/Controller/AdminUserController.php
    	src/AppBundle/Controller/ContractingUserController.php
    	src/AppBundle/Controller/DefaultController.php
    	src/AppBundle/Controller/EconomicUserController.php
    	src/AppBundle/Entity/AdminUser.php
    	src/AppBundle/Entity/ContractingUser.php
    	src/AppBundle/Entity/EconomicUser.php
    	src/AppBundle/Form/Type/AdminUserChangePasswordType.php
    	src/AppBundle/Form/Type/AdminUserProfileType.php
    	src/AppBundle/Form/Type/AdminUserType.php
    	src/AppBundle/Form/Type/BidEvaluationType.php
    	src/AppBundle/Form/Type/BidType.php
    	src/AppBundle/Form/Type/ChangePasswordType.php
    	src/AppBundle/Form/Type/CompanyProfileType.php
    	src/AppBundle/Form/Type/ContactPersonType.php
    	src/AppBundle/Form/Type/ContractDocumentType.php
    	src/AppBundle/Form/Type/ContractEvaluationType.php
    	src/AppBundle/Form/Type/ContractOfficerType.php
    	src/AppBundle/Form/Type/ContractStep1Type.php
    	src/AppBundle/Form/Type/ContractStep2Type.php
    	src/AppBundle/Form/Type/ContractStep3Type.php
    	src/AppBundle/Form/Type/ContractStep4Type.php
    	src/AppBundle/Form/Type/ContractingUserChangePasswordType.php
    	src/AppBundle/Form/Type/ContractingUserProfileType.php
    	src/AppBundle/Form/Type/ContractingUserType.php
    	src/AppBundle/Form/Type/EconomicUserType.php
    	src/AppBundle/Form/Type/PortfolioType.php
    	src/AppBundle/Form/Type/PreQualifiedType.php
    	src/AppBundle/Form/Type/ProfileLogoType.php
    	src/AppBundle/Form/Type/TenderType.php
    	src/AppBundle/Resources/views/Admin/register.html.twig
    	src/AppBundle/Resources/views/Contracting/register.html.twig
    	src/AppBundle/Resources/views/User/payment.html.twig
    	src/AppBundle/Resources/views/User/register.html.twig
    	src/AppBundle/Resources/views/admin.html.twig
    	src/AppBundle/Security/Authorization/Voter/TenderVoter.php
    	web/.htaccess
    	web/app.php
    	web/app_dev.php
    	web/apple-touch-icon.png
    	web/config.php
    	web/css/adminia-responsive.css
    	web/css/adminia.css
    	web/css/bootstrap-datepicker.css
    	web/css/bootstrap-responsive.min.css
    	web/css/bootstrap-table.css
    	web/css/bootstrap-theme.css
    	web/css/bootstrap-theme.min.css
    	web/css/bootstrap.css
    	web/css/bootstrap.min.css
    	web/css/cake.generic.css
    	web/css/dashboard.css
    	web/css/dataTables.bootstrap.min.css
    	web/css/font-awesome.css
    	web/css/font-awesome.min.css
    	web/css/jquery.css
    	web/css/jquery.dataTables.min.css
    	web/css/jquery.datetimepicker.css
    	web/css/login.css
    	web/css/main.css
    	web/css/select.bootstrap.min.css
    	web/favicon.ico
    	web/js/bootstrap-datepicker.js
    	web/js/bootstrap-table.js
    	web/js/bootstrap.js
    	web/js/bootstrap.min.js
    	web/js/dataTables.bootstrap.min.js
    	web/js/dataTables.select.min.js
    	web/js/jquery-2.1.4.min.js
    	web/js/jquery.dataTables.min.js
    	web/js/jquery.datetimepicker.full.min.js
    	web/js/jquery.js
    	web/js/jquery.min.js
    	web/js/moment.js
    	web/robots.txt

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\Payment;
 class DefaultController extends Controller{
     
@@ -16,25 +17,50 @@ class DefaultController extends Controller{
  public function recievePaymentAction(Request $request){
      if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
         $payment = new Payment();
-      
-        $payment->setBusinessNumber($request->request->get('business_number'));
-        $payment->setTransactionReference($request->request->get('transaction_reference'));
-        $payment->setInternalTransactionId($request->request->get('internal_transaction_id'));
-        $payment->setTransactionTimestamp($request->request->get('transaction_timestamp'));
-        $payment->setTransactionType($request->request->get('transaction_type'));
-        $payment->setAccountNumber($request->request->get('account_number'));
-        $payment->setSenderPhone($request->request->get('sender_phone'));
-        $payment->setFirstName($request->request->get('first_name'));
-        $payment->setMiddleName($request->request->get('middle_name'));
-        $payment->setLastName($request->request->get('last_name'));
-        $payment->setAmount($request->request->get('amount'));
-        $payment->setCurrency($request->request->get('currency'));
-        $payment->setSignature($request->request->get('signature'));
+        $data = json_decode($request->getContent(), true);
+        $hashed_value= base64_encode(hash_hmac("sha1", $request->getContent(), "467b0802b92753a97df396b73f65e289d612d2c9", true));
+        $hashed_expected = $data['signature'];
+        
+        if (self::hash_compareAction($hashed_value, $hashed_expected)) {
+        $payment->setBusinessNumber($data['business_number']);
+        $payment->setTransactionReference($data['transaction_reference']);
+        $payment->setInternalTransactionId($data['internal_transaction_id']);
+        $payment->setTransactionTimestamp($data['transaction_timestamp']);
+        $payment->setTransactionType($data['transaction_type']);
+        $payment->setAccountNumber($data['account_number']);
+        $payment->setSenderPhone($data['sender_phone']);
+        $payment->setFirstName($data['first_name']);
+        $payment->setMiddleName($data['middle_name']);
+        $payment->setLastName($data['last_name']);
+        $payment->setAmount($data['amount']);
+        $payment->setCurrency($data['currency']);
+        $payment->setSignature($data['signature']);
         
         $em = $this->getDoctrine()->getManager();
         $em->persist($payment);
         $em->flush();
-     }
+       }else{
+           echo "hashes do not match!";
+           echo  "Expected hashed=".$hashed_expected;
+           echo  "Hashed value=".$hashed_value;
+      }
     }
-}
+ }
+     function hash_compareAction($a, $b) {
+        if (!is_string($a) || !is_string($b)) {
+            return false;
+        }
+       
+        $len = strlen($a);
+        if ($len !== strlen($b)) {
+            return false;
+        }
 
+        $status = 0;
+        for ($i = 0; $i < $len; $i++) {
+            $status |= ord($a[$i]) ^ ord($b[$i]);
+        }
+        return $status === 0;
+    } 
+  
+}
